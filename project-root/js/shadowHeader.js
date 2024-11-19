52dffc57c9f6197b3272b989c71260cccf73d899
inysik <inessa1992@yandex.ru>
2024-11-19 16:01:36 +0300

    commit_add correction

--- project-root/js/shadowHeader.js
+++ project-root/js/shadowHeader.js
@@ -1,8 +1,9 @@
-const header = document.querySelector('.header')
+const headerElement = document.querySelector('.header');
+
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-      header.classList.add('shadow');
+        headerElement.classList.add('shadow');
     } else {
-      header.classList.remove('shadow');
+        headerElement.classList.remove('shadow');
     }
-  })
+});
